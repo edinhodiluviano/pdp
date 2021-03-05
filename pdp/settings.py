e2a5348050dd027529939a5a6a977161245dac74
Edinho <git@final04.com>
2021-03-05 02:12:35 -0300

    adding item pipelines

--- pdp/settings.py
+++ pdp/settings.py
@@ -62,9 +62,9 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'pdp.pipelines.PdpPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'pdp.pipelines.PdpPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
