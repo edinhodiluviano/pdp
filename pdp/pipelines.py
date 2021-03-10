99287e8fc0478684f5e407084d3c1ad0af7a7b3f
Edinho <git@final04.com>
2021-03-09 23:03:19 -0300

    working version

--- pdp/pipelines.py
+++ pdp/pipelines.py
@@ -1,6 +1,5 @@
 import json
 import hashlib
-import subprocess
 import datetime as dt
 from pathlib import Path
 
@@ -15,7 +14,7 @@ def datetime_encoder(obj):
 
 def dict_hash(obj):
     dict_str = json.dumps(
-        item, sort_keys=True, ensure_ascii=False, default=datetime_encoder
+        obj, sort_keys=True, ensure_ascii=False, default=datetime_encoder
     )
     hasher = hashlib.sha256()
     hasher.update(dict_str.encode("utf8"))
@@ -25,7 +24,7 @@ def dict_hash(obj):
 class Check:
     def process_item(self, item, spider):
         if "url" not in item:
-            raise ValueError(f'Item should have a "url" field')
+            raise ValueError('Item should have a "url" field')
         return item
 
 
@@ -37,7 +36,7 @@ class IncludeFields:
     def process_item(self, item, spider):
         new_item = {
             "raw": item,
-            "timestamp": self.datetime.utcnow(),
+            "timestamp": dt.datetime.utcnow(),
             "hash": dict_hash(item),
             "spider": spider.name,
             "version": self.version,
@@ -52,30 +51,8 @@ class Save:
         item_str = json.dumps(
             item, sort_keys=True, ensure_ascii=False, default=datetime_encoder
         )
+        item_str += "\n"
         with open(filename, "a") as f:
-            f.write(item_str + "\n")
+            f.write(item_str)
 
         return item
-
-
-```
-{
-    "raw": {
-        "url": "https://some.domain.com/some/path",
-    },
-    "timestamp": "2021-03-05T05:14:30.301258",
-    "hash": "0123456789ABCDEF",
-    "spider": "some spider name",
-    "version": "0123456789ABCDEF",
-    "crawl": "2021-03-05T05:14:30.301258",
-}
-```
-
-raw: o conteúdo do item, um dicionário, conforme enviado pela spider  
-    url: todos os items já devem vir com uma chave "url"  
-timestamp: o horário que o item foi coletado  
-hash: o sha256 do json do conteúdo do item  
-spider: o nome da spider  
-version: o commit que no qual o código esta sendo executado  
-crawl: o horário que o crawl foi iniciado
-
